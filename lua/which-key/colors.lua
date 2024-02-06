ee261a2e9aa97e3b7d57c532391ddd483b77eb6c
Nick Hoffman <njhoffman1982@gmail.com>
2024-02-06 01:17:18 -0600

    feat: new color highlighting fields

--- lua/which-key/colors.lua
+++ lua/which-key/colors.lua
@@ -1,18 +1,26 @@
+local Config = require("which-key.config")
+
 local M = {}
 
 local links = {
-  [""] = "Function",
+  [""]      = "Function",
   Separator = "Comment",
-  Group = "Keyword",
-  Desc = "Identifier",
-  Float = "NormalFloat",
-  Border = "FloatBorder",
-  Value = "Comment",
+  Group     = "Keyword",
+  Desc      = "Identifier",
+  Float     = "NormalFloat",
+  Value     = "Comment",
+  Border    = "FloatBorder",
+  Mode      = 'Special',
 }
 
 function M.setup()
   for k, v in pairs(links) do
     vim.api.nvim_set_hl(0, "WhichKey" .. k, { link = v, default = true })
+    if vim.tbl_contains(Config.options.mode_highlights, k:lower()) then
+      for _, mode in pairs({ "Normal", "Insert", "Command", "Operator", "Visual" }) do
+        vim.api.nvim_set_hl(0, "WhichKey" .. k .. mode, { link = v, default = true })
+      end
+    end
   end
 end
 
